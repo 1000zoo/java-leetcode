81c9d773ed9e31b0dc942cb1d92190593e50f977
1000zoo <cjswldn99@gmail.com>
2022-08-24 22:06:08 +0900

    leetcode / stack 1047 / Remove All Adjacent Duplicates In String / 2022-08-03

---
+++ java_leetcode/src/stack/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+//2022-08-03
+
+package stack;
+
+import java.util.Stack;
+
+public class RemoveAllAdjacentDuplicatesInString {
+
+    //Runtime: 96 ms, faster than 46.51% of Java online submissions for Remove All Adjacent Duplicates In String.
+    //Memory Usage: 54.7 MB, less than 53.79% of Java online submissions for Remove All Adjacent Duplicates In String.
+    public static String solution1(String s) {
+        StringBuilder ans = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!st.isEmpty() && st.peek() == s.charAt(i)) {
+                st.pop();
+            } else {
+                st.push(s.charAt(i));
+            }
+        }
+
+        while (!st.isEmpty()) {
+            ans.append(st.pop());
+        }
+        ans.reverse();
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "asddsadf";
+        System.out.println(solution1(s));
+    }
+}
