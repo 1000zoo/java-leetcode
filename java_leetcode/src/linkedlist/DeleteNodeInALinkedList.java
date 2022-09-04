5fed8efd52e575eda49cfa5a13c8a03dc9b78460
1000zoo <cjswldn99@gmail.com>
2022-09-05 00:09:53 +0900

    leetcode / linked list 0237 / Delete Node In a Linked List / 2022-09-04

---
+++ java_leetcode/src/linkedlist/DeleteNodeInALinkedList.java
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/delete-node-in-a-linked-list/
+//2022-09-04
+
+package linkedlist;
+
+import java.util.List;
+
+public class DeleteNodeInALinkedList {
+    static public class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Delete Node in a Linked List.
+    //Memory Usage: 43.9 MB, less than 48.01% of Java online submissions for Delete Node in a Linked List.
+    public void solution1(ListNode node) {
+        while (true) {
+            node.val = node.next.val;
+            if (node.next.next == null) {
+                node.next = null;
+                break;
+            }
+            node = node.next;
+        }
+    }
+
+    //Leetcode solution
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Delete Node in a Linked List.
+    //Memory Usage: 41.9 MB, less than 91.76% of Java online submissions for Delete Node in a Linked List.
+    public void solution2(ListNode node) {
+        ListNode nextNode = node.next;
+        node.val = nextNode.val;
+        node.next = nextNode.next;
+        nextNode.next = null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
