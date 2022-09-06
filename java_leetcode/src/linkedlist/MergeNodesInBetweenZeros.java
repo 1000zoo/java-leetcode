82eb424f89b7cf3397373ebcce38de30e268ae88
1000zoo <cjswldn99@gmail.com>
2022-09-06 20:14:54 +0900

    leetcode / linked list 2181 / Merge Nodes in Between Zeros

---
+++ java_leetcode/src/linkedlist/MergeNodesInBetweenZeros.java
@@ -0,0 +1,70 @@
+//https://leetcode.com/problems/merge-nodes-in-between-zeros/
+//2022-09-06
+
+package linkedlist;
+
+import java.util.ArrayList;
+
+public class MergeNodesInBetweenZeros {
+
+    //Runtime: 14 ms, faster than 34.38% of Java online submissions for Merge Nodes in Between Zeros.
+    //Memory Usage: 298.2 MB, less than 48.43% of Java online submissions for Merge Nodes in Between Zeros.
+    public static ListNode solution1(ListNode head) {
+        if (head.val != 0 || head.next == null) {
+            return null;
+        }
+        int mergeSum = 0;
+        ListNode answer = new ListNode();
+        ListNode answerCurr = answer;
+        ListNode curr = head.next;
+        while (curr != null) {
+            if (curr.val != 0) {
+                mergeSum += curr.val;
+            } else {
+                answerCurr.next = new ListNode(mergeSum);
+                answerCurr = answerCurr.next;
+                mergeSum = 0;
+            }
+            curr = curr.next;
+        }
+        return answer.next;
+    }
+
+    //Runtime: 15 ms, faster than 26.03% of Java online submissions for Merge Nodes in Between Zeros.
+    //Memory Usage: 303.1 MB, less than 38.07% of Java online submissions for Merge Nodes in Between Zeros.
+    public static ListNode solution2(ListNode head) {
+        if (head.val != 0 || head.next == null) {
+            return null;
+        }
+        int mergeSum = 0;
+        ListNode answer = new ListNode();
+        ListNode answerCurr = answer;
+        ListNode curr = head.next;
+        while (curr != null) {
+            mergeSum += curr.val;
+            if (curr.next.val == 0) {
+                curr = curr.next.next;
+                answerCurr.next = new ListNode(mergeSum);
+                answerCurr = answerCurr.next;
+                mergeSum = 0;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return answer.next;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arl = new ArrayList<>();
+        arl.add(0);
+        arl.add(1);
+        arl.add(2);
+        arl.add(0);
+        arl.add(3);
+        arl.add(4);
+        arl.add(0);
+        ListNode n = new ListNode(arl);
+        n = n.next;
+        System.out.println(solution2(n));
+    }
+}
