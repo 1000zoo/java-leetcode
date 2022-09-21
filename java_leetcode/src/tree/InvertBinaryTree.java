4e44e07cbe98053f780276031524aee15d60545d
1000zoo <cjswldn99@gmail.com>
2022-09-21 18:40:14 +0900

    leetcode / tree 0226 / Invert Binary Tree

---
+++ java_leetcode/src/tree/InvertBinaryTree.java
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/invert-binary-tree/
+//2022-09-21
+
+package tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class InvertBinaryTree {
+
+    //solution1
+    //recursive
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Invert Binary Tree.
+    //Memory Usage: 41.6 MB, less than 48.75% of Java online submissions for Invert Binary Tree.
+    public static TreeNode invertTree(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    private static void dfs(TreeNode node) {
+        if (node.right == null && node.left == null) {
+            return;
+        }
+        if (node.left != null) {
+            dfs(node.left);
+        }
+        if (node.right != null) {
+            dfs(node.right);
+        }
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        Integer[] list = new Integer[]{4, 2};
+        Collections.addAll(al, list);
+        invertTree(new TreeNode(al));
+    }
+}
