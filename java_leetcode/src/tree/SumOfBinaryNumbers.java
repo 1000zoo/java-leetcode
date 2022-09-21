6b86d3f18fcfc434575fbbcd9c647a014deaabdb
1000zoo <cjswldn99@gmail.com>
2022-09-21 18:22:17 +0900

    leetcode / tree 1022 / Sum of Root to Leaf Binary Numbers

---
+++ java_leetcode/src/tree/SumOfBinaryNumbers.java
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+//2022-09-21
+
+package tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SumOfBinaryNumbers {
+
+    //solution1
+    //recursive
+    //==============================================================================================================
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Sum of Root To Leaf Binary Numbers.
+    //Memory Usage: 41.7 MB, less than 92.79% of Java online submissions for Sum of Root To Leaf Binary Numbers.
+    public static int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0, 0);
+    }
+
+    public static int dfs(TreeNode node, int sum, int num) {
+        num = (num << 1) | node.val;
+        if (node.left == null && node.right == null) {
+            return num;
+        } else if (node.left == null) {
+            sum += dfs(node.right, sum, num);
+        } else if (node.right == null) {
+            sum += dfs(node.left, sum, num);
+        } else {
+            sum += dfs(node.left, sum, num) + dfs(node.right, sum, num);
+        }
+
+        return sum;
+    }
+    //==============================================================================================================
+
+    public static void main(String[] args) {
+        ArrayList<Integer> al1 = new ArrayList<>();
+        Integer[] arr1 = new Integer[]{1, 0, 1, 0, 1, 0, 1};
+        Collections.addAll(al1, arr1);
+        TreeNode root = new TreeNode(al1);
+        System.out.println(sumRootToLeaf(root));
+    }
+}
