d28d4d40ad0fb8d4af634782a788d2f1c4ad3404
1000zoo <cjswldn99@gmail.com>
2022-10-10 12:09:42 +0900

    주석 추가

--- java_leetcode/src/tree/MinimumDistanceBetweenBST.java
+++ java_leetcode/src/tree/MinimumDistanceBetweenBST.java
@@ -13,8 +13,8 @@ public class MinimumDistanceBetweenBST {
     //BST 를 정렬한 후, 바로 다음 원소와 차이를 비교하여 최솟값을 찾는 방식
     //BST 를 중위탐색하면 정렬
     //==================================================================================================================
-    //Runtime: 1 ms, faster than 43.14% of Java online submissions for Minimum Distance Between BST Nodes.
-    //Memory Usage: 41.6 MB, less than 62.41% of Java online submissions for Minimum Distance Between BST Nodes.
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Distance Between BST Nodes.
+    //Memory Usage: 40 MB, less than 88.03% of Java online submissions for Minimum Distance Between BST Nodes.
     public int solution1(TreeNode root) {
         ArrayList<Integer> sort = new ArrayList<>();
         toList(root, sort);
