3f9b3eaedf6b4fdee0a8e3324784292ad5bc3d26
1000zoo <cjswldn99@gmail.com>
2022-10-19 14:37:34 +0900

    leetcode / dynamic programming 0121 / Best Time to Buy and Sell Stock

--- java_leetcode/src/dynamic_programming/BestTimeToBuyAndSellStock.java
+++ java_leetcode/src/dynamic_programming/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,6 @@
-//2022-01-04
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+//2022-01-04
+//retry 2022-10-19
 
 package dynamic_programming;
 
@@ -8,40 +9,39 @@ import java.util.List;
 
 public class BestTimeToBuyAndSellStock {
 
+    //solution1
+    //가장 쉽게 생각할수 있는 O(N^2)
     //Time Limit Exceeded
-    private boolean isIncrease(int init, int fin){
-        return fin > init;
+    public int solution1(int[] prices) {
+        int max = Integer.MIN_VALUE;
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                profit = prices[j] - prices[i];
+                max = Math.max(profit, max);
+            }
+        }
+        return Math.max(max, 0);
     }
-    public int maxProfit(int[] prices){
-        if(prices.length == 1) return 0;
-        boolean isLinear = true;
-        boolean postState = isIncrease(prices[0], prices[1]);
-        boolean initState = postState;
-        int maxProfit = 0;
-        List<Integer> localValue = new ArrayList<>();
-        localValue.add(prices[0]);
-        for(int i = 1; i < prices.length - 1; i++){
-            if(postState ^ isIncrease(prices[i], prices[i + 1])){
-                localValue.add(prices[i]);
-                postState = !postState;
-                isLinear = false;
-            }
-        }
-        if(isLinear && postState) return prices[prices.length - 1] - prices[0];
-        localValue.add(prices[prices.length - 1]);
 
-        for(int i = 0; i < localValue.size() - 1; i=i+2){
-            for(int j = i + 1; j < localValue.size(); j=j+2){
-                if(initState){
-                    maxProfit = Math.max(localValue.get(j) - localValue.get(i),
-                            maxProfit);
-                }
-                else{
-                    maxProfit = Math.max(localValue.get(i) - localValue.get(j),
-                            maxProfit);
-                }
+    //refer: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/1735550/Python-Javascript-Easy-solution-with-very-clear-Explanation
+    //solution2
+    //매수 시점 보다 매도 시점의 값이 클 때만 매수시점을 변경해줌
+    //Runtime: 3 ms, faster than 76.30% of Java online submissions for Best Time to Buy and Sell Stock.
+    //Memory Usage: 83.4 MB, less than 57.32% of Java online submissions for Best Time to Buy and Sell Stock.
+    public int solution2(int[] prices) {
+        int buyTime = 0;
+        int sellTime = 1;
+        int profit = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (; sellTime < prices.length; sellTime++) {
+            profit = prices[sellTime] - prices[buyTime];
+            max = Math.max(profit, max);
+            if (profit < 0) {
+                buyTime = sellTime;
             }
         }
-        return maxProfit;
+        return Math.max(max, 0);
     }
 }
