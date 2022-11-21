7ceef338f6886a6ea892b2b1bb263811b790a1ba
1000zoo <cjswldn99@gmail.com>
2022-11-21 19:51:48 +0900

    leetcode / greedy 1221 easy / Split a String in Balanced Strings

---
+++ java_leetcode/src/greedy/SplitStingInBalancedStrings.java
@@ -0,0 +1,15 @@
+package greedy;
+
+public class SplitStingInBalancedStrings {
+
+    //Runtime: 1 ms, faster than 81.57% of Java online submissions for Split a String in Balanced Strings.
+    //Memory Usage: 41.6 MB, less than 75.00% of Java online submissions for Split a String in Balanced Strings.
+    public int solution(String s) {
+        int answer = 0, count = 0;
+        for (char c : s.toCharArray()) {
+            count += c == 'L' ? 1 : -1;
+            if (count == 0) answer++;
+        }
+        return answer;
+    }
+}
