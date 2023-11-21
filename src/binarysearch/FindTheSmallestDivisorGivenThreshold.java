d10705641d8f0e5e654923f2e17840de93c957ee
1000zoo <cjswldn99@gmail.com>
2023-11-21 18:06:26 +0900

    leetcode / medium / binary search / Find the Smallest Divisor Given a Threshold

---
+++ src/binarysearch/FindTheSmallestDivisorGivenThreshold.java
@@ -0,0 +1,42 @@
+//2023-11-21
+//https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
+
+package binarysearch;
+
+public class FindTheSmallestDivisorGivenThreshold {
+
+    //Runtime: 28ms Beats 71.59%
+    public int smallestDivisor(int[] nums, int threshold) {
+        int left = 1;
+        int right = getMax(nums);
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            long result = getSum(nums, mid);
+
+            if (result <= threshold) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private int getMax(int[] nums) {
+        int max = 0;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    private long getSum(int[] nums, int divisor) {
+        long sum = 0;
+        for (int num : nums) {
+            sum += (int) Math.ceil((double) num / divisor);
+        }
+        return sum;
+    }
+}
