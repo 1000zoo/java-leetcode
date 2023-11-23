a7cb60e30985ce4cc02d9a30b93acfa259e2146f
1000zoo <cjswldn99@gmail.com>
2023-11-23 18:22:49 +0900

    leetcode / medium / dynamic programming / Unique Pahts

---
+++ src/dynamic_programming/UniquePaths.java
@@ -0,0 +1,32 @@
+//2023-11-23
+//https://leetcode.com/problems/unique-paths/
+
+package dynamic_programming;
+
+import java.util.*;
+
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int[] d : dp) {
+            Arrays.fill(d, Integer.MAX_VALUE);
+        }
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 1;
+        }
+        Arrays.fill(dp[0], 1);
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (dp[i][j - 1] == Integer.MAX_VALUE || dp[i - 1][j] == Integer.MAX_VALUE) {
+                    continue;
+                }
+                dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + dp[i][j - 1]);
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
